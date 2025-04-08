30ee6601a2b01060209ba7b9e48a3abfc5b8fed7
rinusantoro <rinusantoro@gmail.com>
2025-04-09 02:22:17 +0700

    Addition two number

---
+++ 1_Penjumlahan.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	char nama[ ]     = "Sonasa Rinusantoro";
+    char kodedosen[] = "02289";
+
+    int a, b, hasil;
+
+    // Input angka pertama
+    cout << "Masukan angka pertama : ";
+    cin >> a;
+
+    // Input angka kedua
+    cout << "Masukan angka kedua : ";
+    cin >> b;
+
+    // Penjumlahan angka
+    hasil = a + b;
+
+    // Output hasil penjumlahan
+    cout << "Hasil Penjumlahan a dan b : " << hasil << endl;
+    
+
+    // Output nama dan kode dosen
+     cout << "Nama: "<< nama << endl;
+     cout << "kode Dosen: " << kodedosen << endl;
+
+   // return 0;
+}
