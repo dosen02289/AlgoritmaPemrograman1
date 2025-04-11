1ca8f1ad3d91737c2ece9b6e4a4d8a3cdcff1e9e
rinusantoro <rinusantoro@gmail.com>
2025-04-11 19:53:21 +0700

    Added two variabel

---
+++ 5_variabel_02.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+
+main()
+{ 
+    int x, y; //deklarasi variabel x dan y dengan tipe data int 
+    x = 15; //mengisi nilai variabel x dengan nilai 15
+    y = 50; //mengisi nilai variabel y dengan nilai 50
+    cout << x << endl; //mencetak isi variabel x 
+    cout << y; //mencetak isi variabel y
+}
