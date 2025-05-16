b97a7dc417668ac859fed6170bfb8f1f698a5fb1
Rinu <sonasa@ecampus.ut.ac.id>
2025-05-16 19:56:03 +0700

    Add files via upload

---
+++ 11_Deret.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int deret;
+cout << "Masukan jumlah deret: ";
+cin >> deret;
+cout << "Jumlah deretnya adalah:";
+cout << endl;
+
+while(deret>0){
+ cout << deret << " ";
+ deret--;
+}
+
+return 0;
+}
